ac397c3b694f814f498ce99932f99237e531fb88
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2025-07-22 15:44:21 -0300

    style: add gap

--- src/Layouts/Header/Header.tsx
+++ src/Layouts/Header/Header.tsx
@@ -4,10 +4,10 @@ import { CiSearch } from "react-icons/ci";
 
 export default function Header() {
     return (
-        <header className="flex items-center justify-between p-5 w-full fixed z-10 bg-white/10 backdrop-blur-md">
+        <header className="flex items-center justify-between p-5 w-full fixed z-10 bg-white/10 backdrop-blur-md gap-5">
             <h1 className="text-white text-3xl font-bold ">CineVerse</h1>
             <div className="flex items-center gap-2">
-                <Input type="text" placeholder="Buscar por..." className="w-full max-w-[500px]" />
+                <Input type="text" placeholder="Buscar por..." className="w-full max-w-[500px] text-white" />
                 <Button className="text-white">
                     <CiSearch />
                 </Button>
