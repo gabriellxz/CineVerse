afa9c714a65b5a53b55cc1ff0f0e1a36c9631da8
gabriellxz <gabrielsilvaoliveira521@gmail.com>
2025-08-03 22:22:14 -0300

    header style responsive

--- src/Layouts/Header/Header.tsx
+++ src/Layouts/Header/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     const navigate = useNavigate()
 
     return (
-        <header className="flex items-center justify-between p-5 w-full fixed z-20 bg-white/10 backdrop-blur-md gap-5">
+        <header className="flex items-center justify-between p-5 w-full fixed z-20 md:bg-white/10 md:backdrop-blur-md gap-5">
             <div className="flex items-center gap-2">
                 <h1 className="text-white text-3xl font-bold ">CineVerse</h1>
             </div>
